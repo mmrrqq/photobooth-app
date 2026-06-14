3885036e64012d9fc8a05a275053884d87629d26
markus rothgaenger <mrothgaenger@techfak.uni-bielefeld.de>
2026-06-14 16:41:34 +0200

    wip add template

--- src/photobooth/utils/image.py
+++ src/photobooth/utils/image.py
@@ -1,24 +1,24 @@
+from pathlib import Path
 from typing import Self
-from PIL import Image
-from numpy import typing as npt
 
 import numpy as np
-
-from pathlib import Path
+from numpy import typing as npt
+from PIL import Image
 
 
 # TODO: warning logs
-class LabelImage():
+class LabelImage:
     data: npt.NDArray
     image: Image.Image
 
     def __init__(self) -> Self:
         pass
 
+    @staticmethod
     def from_path(path: Path) -> Self:
         if not path.exists() or not path.is_file():
             return
-        
+
         img = LabelImage()
         img.image = Image.open(path)
 
@@ -27,29 +27,24 @@ class LabelImage():
     def _scale(self, target_width: int):
         if self.image is None:
             return
-        
+
         current_width, current_height = self.image.size
         ratio = current_width / current_height
-        target_height = int(np.floor((1/ratio) * target_width))
-        
+        target_height = int(np.floor((1 / ratio) * target_width))
+
         self.image = self.image.resize((target_width, target_height))
 
         return target_height
-        
 
     def _convert_grayscale(self):
         if self.image is None:
             return
-                
-        self.image = self.image.convert("L").convert("1", dither=Image.Dither.FLOYDSTEINBERG)        
+
+        self.image = self.image.convert("L").convert("1", dither=Image.Dither.FLOYDSTEINBERG)
 
     def get_binary_data(self):
         if self.image is None:
-            return        
+            return
 
         image_bytes = np.packbits(np.invert(self.image)).tobytes()
         return image_bytes
-        
-
-
-
--- src/photobooth/utils/print_zebra.py
+++ src/photobooth/utils/print_zebra.py
@@ -14,9 +14,16 @@ LABEL_WIDTH = 80  # mm
 def main():
     path = Path(sys.argv[1])
     label: LabelImage = LabelImage.from_path(path)
+    pre_label: LabelImage = LabelImage.from_path(Path(__file__).parent / "receipt_template.png")
 
     target_width = int(np.floor(LABEL_WIDTH * TO_INCH * DPI))
     target_width -= target_width % 8
+
+    pre_label_height = pre_label._scale(target_width=target_width)
+    assert pre_label_height is not None
+    pre_label._convert_grayscale()
+    pre_label_data = pre_label.get_binary_data()
+    assert pre_label_data is not None
 
     target_height = label._scale(target_width=target_width)
     assert target_height is not None
@@ -26,9 +33,10 @@ def main():
 
     printer = ZebraPrinter()
     printer.set_compensation_mode(True, False, True)
-    printer.set_print_speed(80)
+    printer.set_print_speed(50)
     printer.set_burn_time(570)
     printer.set_secondary_burn_time(120)
+    printer.write_image_rows(pre_label_data, row_size=int(target_width / 8), height=pre_label_height)
     printer.write_image_rows(data, row_size=int(target_width / 8), height=target_height)
     printer.cut()
     printer.print()
